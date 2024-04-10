3c9b7498979bb80871e917f675ca81c9a21f233d
JonyUmul <143048229+JonyUmul@users.noreply.github.com>
2024-04-09 21:51:39 -0600

    Add files via upload
    actualizado

--- src/routes/controlProcesos/detallados/DTH.routes.js
+++ src/routes/controlProcesos/detallados/DTH.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  postDTH, getDTH } from "../../../../controllers/controlProcesos/detalle/DTH.controllers.js";
+import {  postDTH, getDTH, getSDTH } from "../../../../controllers/controlProcesos/detalle/DTH.controllers.js";
 
 
 
@@ -8,5 +8,6 @@ const router = Router();
 
 router.post('/DTH', postDTH);
 router.get('/DTH/:id', getDTH);
+router.get('/DTH/:fecha_creacion/:id_asrd/:id_horno', getSDTH);
 
 export default router;
